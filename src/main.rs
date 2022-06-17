c5e7070b54ef6540e09b31eb7c09969b95ad1b69
LocDT <locdt@Nguyens-Mac-mini.local>
2022-06-17 15:28:57 +0700

    complete 3.3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
-    const a: String = "Hello, world!".to_string();
-println!("{}", a[1]);
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
