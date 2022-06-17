ed2d06ad390a8d6d2f66510721c07d77d5df50cb
LocDT <locdt@Nguyens-Mac-mini.local>
2022-06-17 15:06:56 +0700

    complete 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
 fn main() {
-    const a: String = "Hello, world!".to_string();
-println!("{}", a[1]);
+    // Imutable variable
+    let x = 6;
+    // x = 7; // error: cannot assign to immutable variable `x`
+    println!("{}", x);
+    // Mutable variable
+    let mut y = 6;
+    y = 7; // You can assign to mutable variables
+    println!("{}", y);
+
+    // Constants
+    const THREE: i32 = 3;
+    print!("{}", THREE);
+    // You can declare a immutable and assign a value to it
+    // but you can not declare a const variable
+    // if you no assign a value
+
+    // Shadowing
+    let x = 5;
+    let x = x + 1;
+
+    // Shadowing different with mutable variable is: it can assign a value with different type
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
 }
