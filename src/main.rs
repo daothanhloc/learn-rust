4619182e147e7d2a04121e4f962bec98092e5c0b
LocDT <locdt@Nguyens-Mac-mini.local>
2022-06-30 11:34:52 +0700

    Complete 4

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,49 @@
 // }
 
 // Ownership of function
-fn main() {
-    let s1 = String::from("hello");
+// fn main() {
+//     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+//     let (s2, len) = calculate_length(s1);
 
-    println!("The length of '{}' is {}.", s2, len);
-}
+//     println!("The length of '{}' is {}.", s2, len);
+// }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() trả về độ dài của một String
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len(); // len() trả về độ dài của một String
 
-    (s, length)
-}
+//     (s, length)
+// }
+
+// ======================
+// Borrowing
+// fn main() {
+//     let s1 = String::from("hello");
+//     let len = calculate_length(&s1);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
+// Borrowing mutable
+
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     change(&mut s);
+// }
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+// // Slices
+// fn main() {
+//     let s = String::from("hello");
+
+//     let len = s.len();
+
+//     let slice = &s[0..len];
+//     let slice = &s[..];
+// }
