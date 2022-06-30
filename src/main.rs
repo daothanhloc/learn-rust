8c19344943ccd9e084523a1cb0375ae38e4401f8
LocDT <locdt@Nguyens-Mac-mini.local>
2022-06-30 11:04:53 +0700

    1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+// fn main() {
+//     // Ownership of variables
+//     let x = 5;
+//     let y = x;
+
+//     let a = String::from("hello");
+//     let b = a;
+//     print!("{} - {}", x, y);
+//     println!("{} - {}", a, b); // error: cannot move `a` into `b` because they both require unique ownership
+// }
+
+// Ownership of function
 fn main() {
-    const a: String = "Hello, world!".to_string();
-println!("{}", a[1]);
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() trả về độ dài của một String
+
+    (s, length)
 }
